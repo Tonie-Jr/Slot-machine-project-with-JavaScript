d21a670f37d07354fcc02d87f3b26a0d46be038d
Tonnie <antonymuriithi505@gmail.com>
2024-09-04 13:40:33 +0300

    Add the spin and transpose function to the slot machine project

--- project.js
+++ project.js
@@ -14,7 +14,7 @@ const ROWS = 3;
 const COLS = 3;
 
 //Defining the symbols using objects
-const SYMBOLS_COUNT = {   
+const SYMBOLS_COUNT = {
     A: 2,
     B: 4,
     C: 6,
@@ -75,10 +75,47 @@ const spin = () => {
             symbols.push(symbol);
         }
     }
-    console.log(symbols)
+
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+        reels.push([])
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
 };
 
-spin();
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i])
+        }
+    }
+
+    return rows;
+};
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol
+            if (i != row.length - 1) {
+                rowString += " | "
+            }
+        }
+
+        console.log(rowString)
+    }
+}
 
 let balance = deposit(); // the let allows us to adjust the value, giving us a chance to change what it is stroring future.
 //const numberBet = getBet();
@@ -88,3 +125,8 @@ let balance = deposit(); // the let allows us to adjust the value, giving us a c
 const numberOfLines = getNumberOfLines(); //call the function and assing it a variable name nuberOfLines
 //console.log(numberOfLines) //prints the variabe
 const bet = getBet(balance, numberOfLines);// calls the functiona and assing it avariable name bet
+const reels = spin();
+const rows = transpose(reels);
+console.log(reels);
+console.log(rows);
+printRows(rows);
